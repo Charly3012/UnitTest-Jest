a9c521349f2454cfa15e3100a499bddb31489245
Charly3012 <carlocoronadosilva975@gmail.com>
2026-02-18 21:16:55 -0600

    jest inital config with example

---
+++ src/app.test.ts
@@ -0,0 +1,9 @@
+import { sum } from './app';
+
+describe('sum function', () => {
+
+    it('should return the sum of two numbers', () => {
+        expect(sum(2, 3)).toBe(5);
+    });
+
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 const name: string = 'patroclo';
 console.log(`Falta ${name}...`);
+
+
